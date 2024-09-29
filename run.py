9bf7353094514db0d263e0e9f0d17441d5fbb1ee
Fergus Stonehouse <fergus.m.stonehouse@gmail.com>
2024-09-29 09:19:01 +0000

    Add function to get user choice for game size

--- run.py
+++ run.py
@@ -1,6 +1,3 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
 def get_player_name():
     """
     Function to get the player's name and return it to the game
@@ -12,7 +9,7 @@ def get_player_name():
         print("Let's stick to letters for the time being, Mr Musk...")
         return get_player_name()                        # If letters haven't been used, return to start of function
 
-def get_game_size():
+def game_size_option():
     """
     Function to provide the user the choice of game board size
     """
@@ -20,7 +17,22 @@ def get_game_size():
     print('Press 1 for 4 x 4')
     print('Press 2 for 5 x 5')
     print('Press 3 for 6 x 6')
-    game_size = input("1 / 2 / 3? ")
+    game_size_option = input("1 / 2 / 3? ")
+    try:                                                # ensure the player is inputting a number 1 - 3
+        game_size_option = int(game_size_option)
+        if 1<= game_size_option <=3:
+            return game_size_option
+        else:
+            print('Please choose 1, 2 or 3 instead.')
+            return game_size_option()                   # if input outside numerical options, loop to function start
+    except ValueError:
+        print("Let's stick to whole numbers, please...")
+        return game_size_option()                       # if input isn't a number, raise error and restart function
+    
+def get_game_size(game_size_option):
+    if game_size_option = 1:
+        game_size = 
+    
     return game_size
 
 def get_ship_num():
@@ -44,6 +56,6 @@ def read_to_play():
 player_one = get_player_name()
 game_size = get_game_size()
 ship_num = get_ship_num()
-start_game = read_to_play()
 
 print(f'Okay, {player_one}, you want a {game_size} with {ship_num} ships. Are you ready? {start_game}')
+start_game = read_to_play()
